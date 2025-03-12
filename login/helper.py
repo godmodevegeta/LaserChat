34a43e8530d04c73077122ec121920c2b46403d1
godmodevegeta <kshubham1904@gmail.com>
2025-03-12 21:58:58 +0530

    Refactor helper functions to specify return types for improved type safety

--- login/helper.py
+++ login/helper.py
@@ -1,7 +1,7 @@
 import json
 from typing import Optional
 
-def load_temp_db() -> list:
+def load_temp_db() -> list[dict]:
     with open('users.json') as f:
         users = json.load(f)
     return users
@@ -10,7 +10,7 @@ def write_temp_db(users: list) -> None:
     with open('users.json', 'w') as f:
         json.dump(users, f)
 
-def validate_signup_body(data: Optional[dict]) -> list:
+def validate_signup_body(data: Optional[dict]) -> list[bool, str]:
     # if not all(key in data for key in ['username', 'password', 'email']):
     #     return jsonify({'error': 'Missing required fields'}), 400
     for key in ['email', 'username', 'password']:
@@ -18,7 +18,7 @@ def validate_signup_body(data: Optional[dict]) -> list:
             return [False, key]
     return [True]
 
-def check_if_user_already_exists(users: list[dict], data: dict) -> list:
+def check_if_user_already_exists(users: list[dict], data: dict) -> list[bool, str]:
     for user in users:
         if user.get('username') == data.get('username'):
             return [False, 'Username']
