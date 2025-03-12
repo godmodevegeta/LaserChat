27743448c904a45638b6c39690d5095a9e960558
godmodevegeta <kshubham1904@gmail.com>
2025-03-12 21:32:52 +0530

    Add test script for user data handling in test.py

---
+++ login/test.py
@@ -0,0 +1,19 @@
+import json
+
+# with open('users.json') as f:
+#     users = json.load(f)
+
+# print(users)
+
+user = {
+        "email": "cat@gmail.com",
+        "username": "cat_1234",
+        "password": "Cat@1234"
+    }
+
+# users.append(user)
+
+# with open('users.json', 'w') as f:
+#     json.dump(users, f)
+
+# print(users)
