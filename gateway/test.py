146c50af3788af2f0e64c90887228f44c0188af2
godmodevegeta <kshubham1904@gmail.com>
2025-03-15 13:34:55 +0530

    Add test script for JWT validation helper function

---
+++ gateway/test.py
@@ -0,0 +1,6 @@
+import helper
+
+# token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNoaXZhbTE5MDQiLCJleHAiOjE3NDIwMjU4MTYsImlhdCI6MTc0MjAyMjIxNn0.i93MbJ0Sy4UMBKnA6jwujx4gSDqVYDM5Ks0-7GcXVjg'
+# response = helper.validate_jwt(token)
+
+# print(response)
