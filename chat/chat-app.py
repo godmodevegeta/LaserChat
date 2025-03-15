268c2e9fee79888ce67ace0478aaa6f632a5caa3
godmodevegeta <kshubham1904@gmail.com>
2025-03-15 12:52:58 +0530

    Add message API endpoint to chat application

--- chat/chat-app.py
+++ chat/chat-app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_chat():
     return "<p>Hello, Chat!</p>"
 
+
+@app.route("/api/v1/message")
+def hello_message():
+    return "<p>Hello, Message!</p>"
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=8002)
