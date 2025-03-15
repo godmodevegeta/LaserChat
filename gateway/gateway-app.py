ee3b28a10318400fbddc46c44ed87ffbd506ac0a
godmodevegeta <kshubham1904@gmail.com>
2025-03-15 12:25:18 +0530

    Implement JWT validation for message API endpoint and forward requests to CHAT Microservice

--- gateway/gateway-app.py
+++ gateway/gateway-app.py
@@ -11,12 +11,24 @@ CHAT_SERVICE_URL = config.get('CHAT_SERVICE_URL')
 @app.before_request
 def authenticate_request():
     """ Intercepts all requests and validates JWT """
-    
+    if request.path.startswith("/api/v1/message"):  # Protect only /message
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
+        token = token.split(" ")[1] # Remove Bearer
+        validation_response = validate_jwt(token)
+        if not validation_response:
+            return jsonify({'message': 'Invalid or expired token'}), 401
+        request.user = validation_response.json()
 
 
 @app.route('/api/v1/message', methods=['POST'])
 def message():
-    pass
+    """ Forwards request to the CHAT Microservice """
+    headers = {"Authorization": request.headers.get("Authorization")}
+    response = requests.post(f"{CHAT_SERVICE_URL}/api/v1/message", headers=headers)
+    return jsonify(response.json()), response.status_code
+    
 
 def validate_jwt(token: str):
     """SENDS jwt to AUTH service for validation"""
